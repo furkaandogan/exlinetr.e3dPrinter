982e2205d4fd78e5a1a366a92051adb9fa65704b
Muhammet Furkan DOĞAN <furkan.dogan@hepsiburada.com>
2020-12-21 02:39:13 +0300

    first commit

---
+++ Marlin/_Statusscreen.h
@@ -0,0 +1,14 @@
+/**
+ * Made with Marlin Bitmap Converter
+ * https://marlinfw.org/tools/u8glib/converter.html
+ *
+ * This bitmap from 48x21 C/C++ data
+ */
+#pragma once
+
+#define STATUS_SCREEN_X 80
+#define STATUS_LOGO_WIDTH 48
+
+const unsigned char status_logo_bmp[] PROGMEM = {
+    
+};
